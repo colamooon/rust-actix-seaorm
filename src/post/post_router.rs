50d97e242ba0d751276c2720399f981025038705
cola <cola@amond.io>
2022-11-11 17:19:22 +0900

    add

--- src/post/post_router.rs
+++ src/post/post_router.rs
@@ -5,24 +5,17 @@ use log::info;
 
 use super::post_handler;
 
-async fn validator(
-    req: ServiceRequest,
-    _credentials: BasicAuth,
-) -> Result<ServiceRequest, (Error, ServiceRequest)> {
-    info!("]-----] validator._credentials [-----[ {:?}", _credentials);
-    info!("]-----] validator.req [-----[ {:?}", req);
-    Ok(req)
-}
+
 
 // this function could be located in a different module
 pub fn post_route(cfg: &mut web::ServiceConfig) {
-    let auth = HttpAuthentication::basic(validator);
+    
     cfg.service(
-        web::scope("/api/v1/post").service(
+        // web::scope("/api/v1/post").service(
             web::resource("")
                 .route(web::get().to(post_handler::get_posts))
                 .route(web::post().to(post_handler::add_post))
-                .wrap(auth),
-        ),
+                ,
+        // ),
     );
 }
--- src/router_config.rs
+++ src/router_config.rs
@@ -3,8 +3,19 @@ use actix_web_httpauth::{extractors::basic::BasicAuth, middleware::HttpAuthentic
 use log::info;
 
 use crate::post;
-
+async fn validator(
+    req: ServiceRequest,
+    _credentials: BasicAuth,
+) -> Result<ServiceRequest, (Error, ServiceRequest)> {
+    info!("]-----] validator._credentials [-----[ {:?}", _credentials);
+    info!("]-----] validator.req [-----[ {:?}", req);
+    Ok(req)
+}
 pub fn config_router(cfg: &mut web::ServiceConfig) {
-    // domain includes: /products/{product_id}/parts/{part_id}
-    cfg.configure(post::post_router::post_route);
+    let auth = HttpAuthentication::basic(validator);
+    cfg.service(
+        web::scope("/api/v1/post")
+            .configure(post::post_router::post_route)
+            .wrap(auth),
+    );
 }
